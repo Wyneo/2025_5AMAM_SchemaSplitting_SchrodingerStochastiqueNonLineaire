4b2a9d001d6df4ac65132a07c400c91037550266
Eowyn Hallereau <eowyn.hallereau@etu.univ-nantes.fr>
2025-11-28 18:50:46 +0100

    Amélioration condition périodique, mais masse semble pas correcte

--- euler_maruyama_scheme.py
+++ euler_maruyama_scheme.py
@@ -41,24 +41,41 @@ alpha = 1
 nx = 2**8
 tau = 0.1
 T = 1
-nt = int(1/0.1)
+nt = int(T/0.1)
 x = np.linspace(0,2*np.pi,nx)
 t = np.linspace(0,T,nt)
 
-u=np.zeros((nt,nx+1))
+u=np.zeros((nt,nx+2))
 u[0,1:nx+1] = u0(x)
-u[0,0]=u[0,-1] #pour condition périodique : ajout factice de la dernière valeure avant la première
-u_vrai=np.zeros((nt,nx))
-u_vrai[0]=u0(x)
+u[0,0] = u[0,nx] #pour condition périodique : ajout factice de la dernière valeure avant la première
+u[0,nx+1] = u[0,1]
+
+u_vrai = np.zeros((nt,nx))
+
 for n in range(1,nt):
-    v = np.append(V(x)[-1],V(x))
+    v = np.append(V(x)[nx-1],V(x))
+    v = np.append(v,V(x)[0])
     deltaW = Wq(n*tau,x)-Wq((n-1)*tau,x)
-    deltaW = np.append(deltaW[-1],deltaW)
-    u[n] = u[n-1] - 1j*np.dot(laplacien(nx+1),u[n-1]) - 1j*tau*np.dot(v,u[n-1]) - 1j*alpha*deltaW
-    u[n,0]=u[n,-1] #pour périodicité
-    u_vrai[n]=np.delete(u[n],0) #stocke pas valeure fictive
+    deltaW = np.append(deltaW[nx-1],deltaW)
+    deltaW = np.append(deltaW,deltaW[1])
+
+    u[n] = u[n-1] - 1j*np.dot(laplacien(nx+2),u[n-1]) - 1j*tau*np.dot(v,u[n-1]) - 1j*alpha*deltaW
+
+    u[n,0] = u[n,nx] #pour périodicité
+    u[n,nx+1] = u[n,1]
+    
+u_vrai=np.delete(u,[0,nx+1],1) #on garde pas valeures fictives
+
+#Augmenter T pour mieux voir la périodicité
+plt.figure(1)
+plt.plot(x,u_vrai[0],x,u_vrai[nt-1])
+plt.legend(["Temps initial", "Temps final"])
+plt.title("Graphique de u en fonction de x")
 
 mass = np.linalg.norm(u_vrai, ord=2, axis=1)
 
-plt.plot(t, mass)
+plt.figure(2)
+plt.plot(t,mass)
+plt.title("Evolution de la masse en fonction de t")
+
 plt.show()
