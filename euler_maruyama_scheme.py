05d68d7f300fc723d7ba4f79054274d440934f71
Eowyn Hallereau <eowyn.hallereau@etu.univ-nantes.fr>
2025-12-12 09:20:59 +0100

    Amélioration processus Wiener

--- euler_maruyama_scheme.py
+++ euler_maruyama_scheme.py
@@ -9,6 +9,7 @@ def u0(x):
 
 def laplacien(nx):
     l=np.diagflat([2 for i in range(nx)])+np.diagflat([-1 for i in range(nx-1)],1)+np.diagflat([-1 for i in range(nx-1)],-1)
+    l=l/(2*np.pi/nx)**2
     return l
 
 def V(x): #external potential
@@ -17,15 +18,15 @@ def V(x): #external potential
         V[i]=3/(5-4*np.cos(x[i]))
     return V
 
-def beta(k,t):
+def beta(t):
     np.random.seed(42)
     n=np.size(t)
     dt = t/n
     dW=np.sqrt(dt)*np.random.normal(0,1,n)
-    W=np.cumsum(dW)
-    return W
+    #W=np.cumsum(dW)
+    return dW
 
-def gamma(k,t):
+def gamma(k):
     return 1/(1+k*k)
 
 def e(k,x):
@@ -33,12 +34,13 @@ def e(k,x):
 
 def Wq(t,x):
     Wq=np.zeros(np.size(t))
-    for k in range(100):
-        Wq = Wq + gamma(k,t)*beta(k,t)*e(k,x)
+    for k in range(1000):
+        Wq = Wq + gamma(k)*np.dot(beta(t),e(k,t)) #NP.DOT ?
+        #print(Wq)
     return Wq
 
 alpha = 1
-nx = 2**8
+nx = 100
 tau = 0.1
 T = 1
 nt = int(T/0.1)
@@ -52,30 +54,40 @@ u[0,nx+1] = u[0,1]
 
 u_vrai = np.zeros((nt,nx))
 
+incrementWiener = Wq(t,x)
+incrementWiener = np.append(incrementWiener[nt-1],incrementWiener) #NX OU NT ?
+incrementWiener = np.append(incrementWiener,incrementWiener[1])
+
 for n in range(1,nt):
     v = np.append(V(x)[nx-1],V(x))
     v = np.append(v,V(x)[0])
-    deltaW = Wq(n*tau,x)-Wq((n-1)*tau,x)
-    deltaW = np.append(deltaW[nx-1],deltaW)
-    deltaW = np.append(deltaW,deltaW[1])
 
-    u[n] = u[n-1] - 1j*np.dot(laplacien(nx+2),u[n-1]) - 1j*tau*np.dot(v,u[n-1]) - 1j*alpha*deltaW
+    u[n] = u[n-1] - 1j*np.dot(laplacien(nx+2),u[n-1]) - 1j*tau*np.dot(v,u[n-1]) - 1j*alpha*incrementWiener[n]
 
     u[n,0] = u[n,nx] #pour périodicité
     u[n,nx+1] = u[n,1]
     
-u_vrai=np.delete(u,[0,nx+1],1) #on garde pas valeures fictives
+u_vrai = np.delete(u,[0,nx+1],1) #on garde pas valeures fictives
 
 #Augmenter T pour mieux voir la périodicité
 plt.figure(1)
 plt.plot(x,u_vrai[0],x,u_vrai[nt-1])
 plt.legend(["Temps initial", "Temps final"])
 plt.title("Graphique de u en fonction de x")
+plt.xlabel("x")
+plt.ylabel("u")
 
-mass = np.linalg.norm(u_vrai, ord=2, axis=1)
+mass = np.linalg.norm(u_vrai, ord=2, axis=1)**2
 
 plt.figure(2)
 plt.plot(t,mass)
 plt.title("Evolution de la masse en fonction de t")
 
 plt.show()
+
+#PAS REINCREMENTER W
+#DIVISER LAPLACIEN PAR H^2
+
+#PLOT MASSE PAS BONNE FORMULE 
+#ek EN FONCTION DE X OU DE T
+#TAILLE DE INCREMENT WIENER
