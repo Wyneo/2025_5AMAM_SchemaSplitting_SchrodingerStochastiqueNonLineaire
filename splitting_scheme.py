a503f5efe63a3c612bb8465a3d303815809fee6a
Eowyn Hallereau <eowyn.hallereau@etu.univ-nantes.fr>
2026-01-27 12:41:25 +0100

    Essaie 1 pour vérifier la solution

--- splitting_scheme.py
+++ splitting_scheme.py
@@ -39,12 +39,15 @@ def Wq(nt, tau, nx, x):
 def noyau_chaleur(dt, nx):
     k = np.fft.fftfreq(nx, d=(2.0*np.pi)/nx) * 2.0*np.pi
     return np.exp(-1j * (k**2) * dt)
+def noyau_chaleur2(dt, x):
+    return 1.0/((4*np.pi*1j*dt)**(1/2)) * np.exp(-np.abs(x)**2 / (4*1j*dt))
+
 
 alpha = 1.0
 nx = 2**8
 tau = 0.1
 T = 1
-nt = int(np.floor(T/tau)) + 1
+nt = int(np.floor(T/tau))
 x = np.linspace(0, 2*np.pi, nx, endpoint=False)
 t = np.linspace(0, tau*(nt-1), nt)
 
@@ -60,26 +63,46 @@ for i in range(nc): # Monte Carlo
     Vx = V(x)
     W = Wq(nt, tau, nx, x)  # shape(nt, nx)
     S = noyau_chaleur(tau, nx)
+    S2 = noyau_chaleur2(tau, nx)
+    #print(np.shape(S), np.shape(S2))
 
     for n in range(1, nt):
         u_prev = u[n-1, 1:nx+1]
         ubis = np.exp(-1j*tau*Vx)*u_prev
         ubis = ubis - 1j*alpha*W[n]
         u_hat = np.fft.fft(ubis)
-        u_lin = np.fft.ifft(S*u_hat)
+        u_lin = np.fft.ifft(S * u_hat)
     
         u[n, 1:nx+1] = u_lin
         u[n, 0] = u[n, nx]
         u[n, nx+1] = u[n, 1]
 
-    u_vrai = u[:, 1:nx+1]
-    mass[i] = np.linalg.norm(u_vrai, ord=2, axis=1)**2
+    #u_vrai = u[:, 1:nx+1]
+    mass[i] = np.linalg.norm(u[:, 1:nx+1], ord=2, axis=1)**2
 
 E_mass = 1/nc * np.sum(mass, axis=0)
 
+# Calcul de la solution exacte pour comparaison
+W = Wq(nt, tau, nx, x)
+u_exacte = np.zeros((nt, nx), dtype=complex)
+u_exacte[0] = u0(x)
+for n in range(1, nt):
+    integrale1 = 0
+    integrale2 = 0
+    ds = tau
+    temp = t[n]
+    for i in range(nt):
+        s = tau*i
+        integrale1 += noyau_chaleur(temp-s, nx)*V(x)*u_exacte[i]*ds
+        integrale2 += noyau_chaleur(temp-s, nx)*u_exacte[i]*W[i] # *u_exacte[i] ????
+        ds += tau
+    u_exacte[n] = noyau_chaleur(temp, nx)*u0(x) - 1j*integrale1 - 1j*alpha*integrale2
+
 plt.figure(1)
-plt.plot(x, np.real(u_vrai[0]), x, np.real(u_vrai[-1]))
-plt.legend(["Temps initial", "Temps final"])
+# plt.plot(x, np.real(u[0,1:nx+1]), x, np.real(u[-1, 1:nx+1]))
+plt.plot(x, np.real(u[0,1:nx+1]), x, np.real(u[-1, 1:nx+1]), x, np.real(u_exacte[-1]), '--')
+# plt.legend(["Temps initial", "Temps final"])
+plt.legend(["Temps initial", "Temps final", "Solution exacte"])
 plt.title("Graphique de u en fonction de x")
 plt.xlabel("x")
 plt.ylabel("u")
