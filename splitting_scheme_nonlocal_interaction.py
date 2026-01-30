1cd4c62b8ac0d662732ec1c1d6844207915c2840
Eowyn Hallereau <eowyn.hallereau@etu.univ-nantes.fr>
2026-01-30 16:52:28 +0100

    Ajout sEXP, et nonlinéarité nonlocal interaction

--- splitting_scheme_nonlocal_interaction.py
+++ splitting_scheme_nonlocal_interaction.py
@@ -10,10 +10,10 @@ def laplacien(nx):
     l=l/(2*np.pi/nx)**2
     return l
 
-def V(x): # external potential
+def V(x): # nonlocal interaction
     V=np.zeros(np.shape(x)[0])
     for k in range(np.shape(x)[0]):
-        V[k]=3.0/(5.0-4.0*np.cos(x[k]))
+        V[k]=np.cos(x[k])
     return V
 
 def beta(nt, nx, tau):
@@ -49,10 +49,9 @@ x = np.linspace(0, 2*np.pi, nx, endpoint=False)
 t = np.linspace(0, tau*(nt-1), nt)
 dx = (2.0*np.pi)/nx
 
-Vx = V(x)
 S = noyau_chaleur(tau, nx)
 
-nc = 500 # nombre de réalisations pour monte carlo
+nc = 100 # nombre de réalisations pour monte carlo
 mass_splitting = np.zeros((nc, nt))
 mass_duhamel = np.zeros((nc, nt))
 mass_sEXP = np.zeros((nc, nt))
@@ -65,7 +64,7 @@ for i in range(nc):
     u[0,0] = u[0,nx]
     u[0,nx+1] = u[0,1]
 
-    u_duhamel = np.zeros((nt, nx+2), dtype=complex)
+    u_duhamel = np.zeros((nt, nx+2), dtype=complex) # Calcul de la solution exacte (formule de Duhamel)
     u_duhamel[0,1:nx+1] = u0(x)
     u_duhamel[0,0] = u_duhamel[0,nx]
     u_duhamel[0,nx+1] = u_duhamel[0,1]
@@ -77,14 +76,13 @@ for i in range(nc):
 
     W = Wq(nt, tau, nx, x) # shape(nt, nx)
 
-    ################ ATTENTION ####################
-    # print(np.linalg.norm(np.abs(u[0, 1:nx+1]))**2)
-    # print(((2.0*np.pi)/nx)*np.sum(np.abs(u[0, 1:nx+1])**2))
-    # print(np.sum(np.abs(u[0, 1:nx+1])**2))
-
     for n in range(1, nt):
         # Schéma de splitting
         u_prev = u[n-1, 1:nx+1]
+        # Vx = np.sum(np.dot(V(t[n-1]-x),np.abs(u_prev)**2)*dx)
+        Vx = np.convolve(np.abs(u_prev)**2, V(x), mode="same")*dx
+        # print(np.shape(V(x)), np.shape(np.abs(u_prev)**2), np.shape(Vx))
+        # u_bis = np.exp(-1j*tau*Vx)*u_prev - 1j*alpha*W[n]
         u_bis = np.exp(-1j*tau*Vx)*u_prev
         u_bis = u_bis - 1j*alpha*W[n]
         u_lin = np.fft.ifft(S*np.fft.fft(u_bis))
@@ -98,15 +96,18 @@ for i in range(nc):
         integrale2 = np.zeros(nx, dtype=complex)
         for l in range(1, n+1):
             s = tau*(l-1)
-            integrale1 += np.fft.ifft(noyau_chaleur(temps-s, nx)*np.fft.fft(Vx*u_duhamel[l-1,1:nx+1])) * tau
+            # Vx_duhamel = np.sum(np.dot(V(temps-x),np.abs(u_duhamel[l-1,1:nx+1])**2)*dx)
+            Vx_duhamel = np.convolve(np.abs(u_duhamel[l-1,1:nx+1])**2, V(x), mode="same")*dx
+            integrale1 += np.fft.ifft(noyau_chaleur(temps-s, nx)*np.fft.fft(Vx_duhamel*u_duhamel[l-1,1:nx+1])) * tau
             integrale2 += np.fft.ifft(noyau_chaleur(temps-s, nx)*np.fft.fft(W[l])) * tau
         u_duhamel[n, 1:nx+1] = np.fft.ifft(noyau_chaleur(temps, nx)*np.fft.fft(u0(x))) - 1j*integrale1 - 1j*alpha*integrale2
         u_duhamel[n, 0] = u_duhamel[n, nx]
         u_duhamel[n, nx+1] = u_duhamel[n, 1]
 
-        # Schéma stochastique exponentiel
         u_sEXP_prev = u_sEXP[n-1, 1:nx+1]
-        u_sEXP_bis = u_sEXP_prev - 1j*tau*Vx*u_sEXP_prev - 1j*alpha*W[n]
+        Vx_sEXP = np.convolve(np.abs(u_sEXP_prev)**2, V(x), mode="same")*dx
+        # Vx_sEXP = np.sum(np.dot(V(t[n-1]-x),np.abs(u_sEXP_prev)**2)*dx)
+        u_sEXP_bis = u_sEXP_prev - 1j*tau*Vx_sEXP*u_sEXP_prev - 1j*alpha*W[n]
         u_sEXP_lin = np.fft.ifft(S*np.fft.fft(u_sEXP_bis))
         u_sEXP[n, 1:nx+1] = u_sEXP_lin
         u_sEXP[n, 0] = u_sEXP[n, nx]
@@ -114,10 +115,17 @@ for i in range(nc):
 
     mass_splitting[i] = dx*np.sum(np.abs(u[:, 1:nx+1])**2, axis=1)
     mass_duhamel[i] = dx*np.sum(np.abs(u_duhamel[:, 1:nx+1])**2, axis=1)
-    mass_sEXP[i] = dx*np.sum(np.abs(u_sEXP[:, 1:nx+1])**2, axis=1)
+    mass_sEXP[i] = dx*np.sum(np.abs(u_sEXP[:,1:nx+1])**2, axis=1)
     # mass_splitting[i] = np.linalg.norm(u[:, 1:nx+1], ord=2, axis=1)**2
     # mass_duhamel[i] = np.linalg.norm(u_duhamel[:, 1:nx+1], ord=2, axis=1)**2
-
+# print(u0(x))
+# print(u[0])
+# somme = 0
+# dx = (2.0*np.pi)/nx
+# for z in range(nx):
+#     somme += np.abs(u[0,z])**2*dx
+# print("somme =",somme)
+# print("sum =", np.sum(np.abs(u[0,1:nx+1])**2))
 E_mass_splitting = 1/nc * np.sum(mass_splitting, axis=0)
 E_mass_duhamel = 1/nc * np.sum(mass_duhamel, axis=0)
 E_mass_sEXP = 1/nc * np.sum(mass_sEXP, axis=0)
@@ -143,7 +151,9 @@ u_det[0,nx+1] = u_det[0,1]
 for m in range(1, nt):
     # Schéma déterministe
     u_prev_det = u_det[m-1, 1:nx+1]
-    u_bis_det = np.exp(-1j*tau*Vx)*u_prev_det
+    Vx_det = np.convolve(np.abs(u_prev_det)**2, V(x), mode="same")*dx
+    # Vx_det = np.sum(np.dot(V(t[m-1]-x),np.abs(u_prev_det)**2)*dx)
+    u_bis_det = np.exp(-1j*tau*Vx_det)*u_prev_det
     u_lin_det = np.fft.ifft(S * np.fft.fft(u_bis_det))
 
     u_det[m, 1:nx+1] = u_lin_det
@@ -185,8 +195,8 @@ plt.subplot(221)
 plt.plot(x, np.real(u[0,1:nx+1]), label="Condition initiale", linewidth=1.5)
 plt.plot(x, np.real(u[-1, 1:nx+1]), "r", label="Solution splitting", linewidth=1.5)
 plt.plot(x, np.real(u_det[-1, 1:nx+1]), "g", label="Solution déterministe", linewidth=1.5)
-plt.plot(x, np.real(u_duhamel[-1, 1:nx+1]), "--r", label="Solution duhamel", linewidth=1.5)
-plt.plot(x, np.real(u_sEXP[-1, 1:nx+1]), ":r", label="Solution sEXP", linewidth=1.5)
+# plt.plot(x, np.real(u_duhamel[-1, 1:nx+1]), "--r", label="Solution duhamel", linewidth=1.5)
+# plt.plot(x, np.real(u_sEXP[-1, 1:nx+1]), ":r", label="Solution sEXP", linewidth=1.5)
 plt.legend(fontsize="small")
 plt.title("Graphique de u en fonction de x")
 plt.xlabel("x")
@@ -196,9 +206,9 @@ plt.grid(True, alpha=0.3)
 plt.subplot(222)
 plt.plot(t, mass_det, "g", label="Cas déterministe (alpha=0)", linewidth=1.5)
 plt.plot(t, E_mass_splitting, "red", label="Espérance stochastique Splitting", linewidth=1.5)
-plt.plot(t, E_mass_duhamel, "--r", label="Espérance stochastique Duhamel", linewidth=1.5)
-plt.plot(t, E_mass_sEXP, ":r", label="Espérance stochastique sEXP", linewidth=1.5)
+# plt.plot(t, E_mass_duhamel, "--r", label="Espérance stochastique Duhamel", linewidth=1.5)
 plt.plot(t, E_mass_theorique, ":k", label="Espérance théorique", linewidth=1.5)
+# plt.plot(t, E_mass_sEXP, ":r", label = "Espérance stochastic sEXP", linewidth=1.5)
 plt.xlabel("Temps")
 plt.ylabel("Masse")
 plt.title("Comparaison des espérances")
@@ -210,7 +220,7 @@ plt.plot(x, np.real(u[0, 1:nx+1]), label="Temps 0", linewidth=1.5)
 plt.plot(x, np.real(u[5, 1:nx+1]), label="Temps "+str(5*tau), linewidth=1.5)
 plt.plot(x, np.real(u[-1, 1:nx+1]), label="Temps "+str(T), linewidth=1.5)
 plt.legend(fontsize="small")
-plt.title("Graphique de Real(u) au fil de t")
+plt.title("Graphique de u au fil de t")
 plt.xlabel("x")
 plt.ylabel("u")
 plt.grid(True, alpha=0.3)
@@ -219,8 +229,8 @@ plt.subplot(224)
 plt.plot(x, np.abs(u[0,1:nx+1]), label="Condition initiale", linewidth=1.5)
 plt.plot(x, np.abs(u[-1, 1:nx+1]), "r", label="Solution splitting", linewidth=1.5)
 plt.plot(x, np.abs(u_det[-1, 1:nx+1]), "g", label="Solution déterministe", linewidth=1.5)
-plt.plot(x, np.abs(u_duhamel[-1, 1:nx+1]), "--r", label="Solution duhamel", linewidth=1.5)
-plt.plot(x, np.abs(u_sEXP[-1, 1:nx+1]), ":r", label="Solution sEXP", linewidth=1.5)
+# plt.plot(x, np.abs(u_duhamel[-1, 1:nx+1]), "--r", label="Solution duhamel", linewidth=1.5)
+# plt.plot(x, np.abs(u_sEXP[-1, 1:nx+1]), ":r", label="Solution sEXP", linewidth=1.5)
 plt.legend(fontsize="small")
 plt.title("Graphique de |u| en fonction de x")
 plt.xlabel("x")
